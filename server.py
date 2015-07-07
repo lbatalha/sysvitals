275f0edbbd20d4818fb87d4a6538c3490556bb42
lbatalha <lfrb25@gmail.com>
2015-07-08 00:33:37 +0100

    print json to page

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-
-from flask import Flask, request
+import json
+from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
 data = "NA"
 
 @app.route('/')
 def main_stats():
+	f = open('data.json')
+	for line in f:
+		json_data = line
+
+	data = json.loads(json_data)
+	
 	return render_template('main_stats.html', data=data)
 
 
 @app.route('/post', methods = ['POST'])
 def post():
-	data = request.json
-	print(data)
-	return str(data)
+	json_data = request.json
+	f = open('data.json', 'a')
+	f.write('\n')
+	json.dump(json_data, f)
+	return str(json_data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
