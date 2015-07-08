835eb689f8c687c88757051aabb2b842ee444921
lbatalha <lfrb25@gmail.com>
2015-07-08 02:30:19 +0100

    refactored json output

--- client.py
+++ client.py
@@ -4,24 +4,44 @@ import psutil
 import time
 import json
 import urllib, urllib2
+import socket
 url = "http://localhost:5000/post"
 
 
 
 while True:
-		
 		print(chr(27) + "[2J")
-		cpu_time = vars(psutil.cpu_times())
-		cpu_percent = psutil.cpu_percent(percpu=True)
-		vmem = vars(psutil.virtual_memory())
-		swap = vars(psutil.swap_memory())
-		diskparts = []
-		for a in psutil.disk_partitions():
-			diskparts.append(vars(a))
 		
-		diskio = vars(psutil.disk_io_counters())
+		cpu, disks, host_info, memory, network, processes, other = [],[],[],[],[],[],[] 
+		
+		host_info.append(socket.gethostname())
+		#host_info.append(
+		cpu.append(vars(psutil.cpu_times()))
+		cpu.append(psutil.cpu_percent(percpu=True))
+		
+		memory.append(vars(psutil.virtual_memory()))
+		memory.append( vars(psutil.swap_memory()))
+		
+		disks.append(vars(psutil.disk_io_counters()))
+		
 	
-		data = json.dumps([ cpu_time, cpu_percent, vmem, swap, diskparts, diskio])
+		if_addr = {}
+		for k, v in psutil.net_if_addrs().iteritems():
+			if_addr[k] = v
+		
+		netio = {}
+		for k, v in psutil.net_io_counters(pernic=True).iteritems():
+			netio[k] = vars(v)
+
+		network.append(if_addr)
+		network.append(netio)
+		
+		diskparts = []
+		for i in psutil.disk_partitions():
+			diskparts.append(vars(i))
+		
+	
+		data = json.dumps([ host_info, cpu, memory, disks, network, processes, other])
 		
 		req = urllib2.Request(url, data, headers={'Content-type': 'application/json'})
 
